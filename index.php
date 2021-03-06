c2f739cac41156cf5078ec65a5f3470c7029e2ea
Grogg19 <differ@list.ru>
2021-03-06 15:03:06 +0300

    Добавлен файл index.php для примера использования компонента

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors',true);
+
+require_once __DIR__ .'/bootstrap.php';
+
+use App\Receiver\GeoLocation;
+use App\Receiver\CurrentWeather;
+use App\Receiver\ForecastWeather;
+use App\Receiver\Ip;
+use App\View;
+
+use function Helpers\renderException;
+
+/**
+ * Simple Example
+ */
+
+try {
+    $current = new CurrentWeather(); // Get data of current weather in this geolocation
+
+    $view = new View('weather', [
+        'title' => 'Прогноз погоды',
+        'current' => $current->getData(),
+        'geoLocation' => $current->getGeolocationData(),
+    ]);
+
+    $view->render();
+
+} catch (\Exception $e) {
+    // if catch Exception make renderException()
+    renderException($e);
+}
+
